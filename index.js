ee4043c9523593b24544f458d3a4a4955d0d51e0
Darcy Buskermolen <darcy@dbitech.ca>
2019-11-16 06:42:32 -0800

    Remove the static documentation, fastify-swagger manages the route clean enough

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const path = require('path')
-const config = require('./config')
-const fastify = require('fastify')()
+const path = require('path');
+const config = require('./config');
+const fastify = require('fastify')();
 
 // compression - add x-protobuf
 fastify.register(
   require('fastify-compress'), {
     customTypes: /^text\/|\+json$|\+text$|\+xml|x-protobuf$/
   }
-)
+);
 
 // CORS
-fastify.register(require('fastify-cors'))
+fastify.register(require('fastify-cors'));
 
-// swagger
+// swagger documentation
 fastify.register(require('fastify-swagger'), {
   exposeRoute: true,
   swagger: config.swagger
-})
+});
 
-// static documentation path
+fastify.get('/map', {schema: {hide: true}}, (req, reply) => {
+  reply.sendFile('ol.html');
+});
+
 fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'documentation'),
+  root: path.join(__dirname, 'public'),
   prefix: '/',
-})
-
+});
 
 // routes
 fastify.register(require('fastify-autoload'), {
   dir: path.join(__dirname, 'routes')
-})
+});
 
 // Launch server
 fastify.listen(config.server.port, (config.server.host || 'localhost'), function (err, address) {
   if (err) {
-    console.log(err)
-    process.exit(1)
+    console.log(err);
+    process.exit(1);
   }
-  console.info(`Server listening on ${address}`)
-})
+  console.info(`Server listening on ${address}`);
+});
